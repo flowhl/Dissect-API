c6f166316634d5813857606f2defe1e5889126d9
Florian Wahl <florianwahl2@gmail.com>
2024-04-07 16:40:57 +0200

    Update Deploy.bat

--- Deploy.bat
+++ Deploy.bat
@@ -12,11 +12,11 @@ set "version=%~2"
 
 :: Docker build
 echo Building Docker image...
-docker build -t flowhl/dissectapi:%version% .
+docker build -t flowhl/dissect-api:%version% .
 
 :: Docker tag
 echo Tagging Docker image...
-docker tag flowhl/dissect:%version% ghcr.io/flowhl/dissect-api:%version%
+docker tag flowhl/dissect-api:%version% ghcr.io/flowhl/dissect-api:%version%
 
 :: Docker push
 echo Pushing Docker image...
